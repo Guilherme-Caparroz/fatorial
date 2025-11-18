ba7145e69262550ebf322e658ad73b10f54d6c87
GUILHERME DE SOUSA CAPARROZ <guilherme.caparroz@etec.sp.gov.br>
2025-11-18 20:28:58 -0300

    código corrigido

--- fatorial.test.js
+++ fatorial.test.js
@@ -1,10 +1,19 @@
-function fatorial(numero) {
-    let f = 1;
-    while (numero > 1) {
-        f *= numero;
-        numero--;
-    }
-    return f;
-}
+const { fatorial } = require('./fatorial');
 
-module.exports = fatorial;
+describe('Função fatorial', () => {
+  test('fatorial de 1', () => {
+    expect(fatorial(1)).toBe(1);
+  });
+
+  test('fatorial de 5', () => {
+    expect(fatorial(5)).toBe(120);
+  });
+
+  test('fatorial de 3', () => {
+    expect(fatorial(3)).toBe(6);
+  });
+
+  test('fatorial de 9', () => {
+    expect(fatorial(9)).toBe(362880);
+  });
+});
